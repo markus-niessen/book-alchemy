8f055236a487be1f8c556bb30cfd158e4339c94f
Markus Nießen <markusniessen1@gmail.com>
2026-07-08 11:57:17 +0200

    Move database initialization outside main block and set port to 5002

--- app.py
+++ app.py
@@ -16,6 +16,9 @@ app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db.init_app(app)
 
+with app.app_context():
+    db.create_all()
+
 
 @app.route("/")
 def home():
@@ -166,7 +169,5 @@ def rate_book(book_id):
 
 
 if __name__ == "__main__":
-    with app.app_context():
-        db.create_all()
 
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5002, debug=True)
